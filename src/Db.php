7760c8b0f5cb2cbb37282c4ed52b6b2dc704b18f
baiy <wo@baiy.org>
2019-07-07 20:06:12 +0800

    优化数据库操作与请求注入方式

---
+++ src/Db.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Baiy\Cadmin;
+
+use Medoo\Medoo;
+
+class Db extends Medoo
+{
+    // 重写执行方法 方便记录sql
+    public function exec($query, $map = [])
+    {
+        $startTime = microtime(true);
+        $result = parent::exec($query, $map);
+        Controller::addListenSql($this->generate($query, $map),[], number_format((microtime(true) - $startTime), 6));
+        return $result;
+    }
+}
